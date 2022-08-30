42cf96a5d05a0c3aee07bb367ec3307241aa1949
Curtrick Walton <curtrickw@yahoo.com>
2022-08-29 23:18:04 -0500

    fixed weather api

--- Frontend/client/src/component/FormInput.js
+++ Frontend/client/src/component/FormInput.js
@@ -19,27 +19,18 @@ export default class FormInput extends Component {
   handleMap = async (e) => {
     e.preventDefault();
     // user search goes here
+    const API = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_KEY}&q=${this.state.city}&format=json`;
     //  third party api
-    let res = await axios
-      .get(
-        `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_KEY}&q=${this.state.city}&format=json`
-      )
-      .catch((err) =>
-        alert("Error something went wrong, Location not found", err)
-      );
-    // console.log(res.data[0]);
-
-    // the lon and lat of the city
-    this.setState({
+try {
+  let res = await axios.get(API);
+      this.setState({
       lon: res.data[0].lon,
       lat: res.data[0].lat,
       display_name: res.data[0].display_name,
-    })
-    try {
-      this.handleWeather(this.state.city, this.state.lon, this.state.lat); // call this method to get our weather data
-    } catch (error) {
-      alert(error.message);
-    }
+    }, () =>this.handleWeather(this.state.city, this.state.lon, this.state.lat))
+} catch (error) {
+  alert("Error something went wrong, Location not found", error)
+}
   };
   // handleWeather = async (cityName) => {
   //   const API = `http://localhost:3001/weather?cityName=${cityName}`; //our URL with query parameters
